6f66b9dad2338e014f50157c0a0c1835d1a07f82
Andrew Jameson <ajameson@swin.edu.au>
2021-08-13 15:23:23 +1000

    Changes required for python3, prevent exceptions when __del__ called during init

--- python/bifrost/psrdada.py
+++ python/bifrost/psrdada.py
@@ -54,15 +54,22 @@ import ctypes
 def get_pointer_value(ptr):
     return ctypes.c_void_p.from_buffer(ptr).value
 
+def _cast_to_bytes(unknown):
+    if type(unknown) is str:
+        return unknown
+    elif type(unknown) is bytes:
+        return unknown
+
 class MultiLog(object):
     count = 0
     def __init__(self, name=None):
         if name is None:
             name = "MultiLog%i" % MultiLog.count
             MultiLog.count += 1
-        self.obj = _dada.multilog_open(name, '\0')
+        self.obj = _dada.multilog_open(_cast_to_bytes(name), _cast_to_bytes('\0'))
     def __del__(self):
-        _dada.multilog_close(self.obj)
+        if hasattr(self, 'obj'):
+            _dada.multilog_close(self.obj)
 
 class IpcBufBlock(object):
     def __init__(self, buf, mutable=False):
@@ -209,7 +216,8 @@ class Hdu(object):
         self.registered = False
     def __del__(self):
         self.disconnect()
-        _dada.dada_hdu_destroy(self.hdu)
+        if hasattr(self, "hdu"):
+            _dada.dada_hdu_destroy(self.hdu)
     def _connect(self, buffer_key=0xDADA):
         self.buffer_key = buffer_key
         _dada.dada_hdu_set_key(self.hdu, self.buffer_key)
